6eaa16840ae3e4df7f792557ebfb9e0ebb98a878
Rafli Fahri <raflianang73@gmail.com>
2024-03-20 00:01:11 +0700

    Tugas Pertemuan 2

---
+++ app/Views/view-latihan1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Tugas Pertemuan 2 -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= esc($nilai1); ?>
+    Nilai 2 = <?= esc($nilai2); ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= esc($nilai1) . " + " . esc($nilai2) . " = " . esc($hasil); ?>
+</body>
+
+</html>
